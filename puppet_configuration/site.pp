915103e5fc9dee2b77b2705f0fe41f614fbf8860
Rachel Graves <rachel.graves@moneyadviceservice.org.uk>
2014-09-16 11:11:42 +0100

    Add direct source link for minecraft

--- puppet_configuration/site.pp
+++ puppet_configuration/site.pp
@@ -1,12 +1,10 @@
+class { 'apt':
+  always_apt_update => true
+}
 class { 'minecraft':
-  source     => 'dev',
+  source     => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar',
   heap_size  => 2048,
   difficulty => 2,
-  motd       => 'Gamer Anonymous',
+  motd       => 'Gamer Anonymous. Please follow our code of conduct at www.gameranonymous.com.  Hosting by ByteMark.',
   ops        => [ 'didlix', 'discomcdisco' ]
-}
-
-class { 'mumble':
-  autostart => true,
-  welcome_text  => 'Gamer Anonymous'
 }
