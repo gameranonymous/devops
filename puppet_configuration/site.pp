0eeaf084c9222d56358d96d1aab4c3312a09f86c
Rachel Graves <rachel@didlix.net>
2018-02-27 13:10:23 +0000

    Fix username

--- puppet_configuration/site.pp
+++ puppet_configuration/site.pp
@@ -7,7 +7,7 @@ class { 'minecraft':
   difficulty => 2,
   motd       => 'Gamer Anonymous. Please follow our code of conduct at www.gameranonymous.com.  Hosting by ByteMark.',
   ops        => [ 'didlix', 'charlotteis' ],
-  white_list_players => [ 'ntlk', 'semanticist', 'rnalexander', 'sgsabbage', 'discomcdisco', 'domdomdomdomdom', 'zeimpossibleface', 'sw17ch', 'bobolous', 'deValmont', 'SamLindenRathen', 'nedraed', 'squishy98', 'skillgannon', 'HellforgeTV' ],
+  white_list_players => [ 'ntlk', 'semanticist', 'rnalexander', 'sgsabbage', 'discomcdisco', 'domdomdomdomdom', 'zeimpossibleface', 'sw17ch', 'bobolous', 'deValmont', 'SamLindenRathen', 'nedraed', 'squishy98', 'Skillgannon', 'HellforgeTV' ],
   level_name => 'Themyscira',
   level_seed => 'Themyscira',
   gamemode => 0,
